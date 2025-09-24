e15b2583433a8ac8bbdfad3f55d4a2271aa8b3ac
arnelagutlicc <135592870+arnelagutlicc@users.noreply.github.com>
2025-09-24 16:09:25 +0200

    Update test_mob5.java

--- test mob/test_mob5.java
+++ test mob/test_mob5.java
@@ -318,6 +318,7 @@ public class AuthenticationHelper {
         saslAuthRequired = enforceAuthEnabled && enforceAuthSchemes.contains(SASL_AUTH_SCHEME);
     }
 
+    // @codescene(disable: "Bumpy Road")
     private void validateConfiguredProperties() {
         if (enforceAuthEnabled) {
             if (enforceAuthSchemes.isEmpty()) {
